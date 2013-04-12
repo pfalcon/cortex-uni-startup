5442a09e1aaf91465cbcd91a6bc3d3874fcc4603
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-04-12 23:39:20 +0300

    Add an example on how to build binaries using cortex-startup.

---
+++ examples/example-no-io.c
@@ -0,0 +1,14 @@
+/*
+ * This is simple example file which does do any I/O and thus
+ * can be compiled and run on any Cortex-M MCU. However. to see
+ * its effect, you'll need to use debugger.
+ */
+
+int var;
+
+void main()
+{
+    while (1) {
+        var ^= 0x55;
+    }
+}
