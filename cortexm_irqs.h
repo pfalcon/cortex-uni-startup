27e37878ebda876446e8238ddc1df609b333295a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-04-22 02:31:19 +0300

    Add header file with IRQ handlers declarations, useful for C++.

---
+++ cortexm_irqs.h
@@ -0,0 +1,29 @@
+/*
+ * Unified Cortex Startup - IRQ handler declarations for C++
+ *
+ * This file is in public domain
+ *
+ * Put together by Paul Sokolovsky based on article by Vanya Sergeev
+ * http://dev.frozeneskimo.com/notes/cortex_cmsis/ , GNU ld documentation
+ * and numerous other public resources.
+ *
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Reset_Handler(void);
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
