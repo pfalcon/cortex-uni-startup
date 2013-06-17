0d1ec1a38da82410f6e1a5415b3654d6d845dc3c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-06-17 21:14:15 +0300

    Support IRQ list with explicit IRQ numbers.
    Useful for MCUs with gaps in IRQ vectors. Don't switch all IRQ list to new
    format, because it's usually easier to diff lists without explicit numbers.
    (It seems there're 2 vendor approaches here: 1) don't use gaps, just renumber
    supported vectors (EFM32), 2) use same numbering for IRQs, just use gaps if
    specific model doesn't support some IRQs (probably most other vendors)).

--- scripts/irqgen.py
+++ scripts/irqgen.py
@@ -1,24 +1,44 @@
+#!/usr/bin/env python
 import sys
 
 
 irqs = []
 f = open(sys.argv[1])
+no = 0
 for l in f:
     l = l.strip()
     if l[0] == "#":
         continue
-    irqs.append(l)
+    fields = [f.strip() for f in l.split()]
+    assert len(fields) <= 2
+    if len(fields) == 2:
+        fields[0] = int(fields[0])
+    else:
+        fields = [no, fields[0]]
+        no += 1
+    irqs.append(fields)
 
 print '#include "startup.c"'
 print
 
 for i in irqs:
-    print 'void %s_IRQHandler(void) ALIAS("Dummy_Handler");' % i
+    print 'void %s_IRQHandler(void) ALIAS("Dummy_Handler");' % i[1]
 
 
 print
 
 print 'void *vendor_vector_table[] __attribute__ ((section(".vendor_vectors"))) = {'
+no = 0
 for i in irqs:
-    print '%s_IRQHandler,' % i
+    if no != i[0]:
+        # Gap detected
+        assert no < i[0]
+        while no < i[0]:
+            print '0,'
+            no += 1
+
+    assert i[0] == no
+    print '%s_IRQHandler,' % i[1]
+    no += 1
+
 print '};'
